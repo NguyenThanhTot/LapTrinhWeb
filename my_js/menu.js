277396d1bab40950f27fa21e39d6263246541c5b
NguyenThanhTot <19130238@st.hcmuaf.edu.vn>
2021-12-11 21:54:25 +0700

    update menu js scroll and solve button search

--- my_js/menu.js
+++ my_js/menu.js
@@ -25,5 +25,41 @@ document.addEventListener("DOMContentLoaded", function ()
     button_cart_mobile_close.onclick = function () { close_cart_right(); };
     bg_close_cart.onclick = function () { close_cart_right(); };
 
-    //Xu ly nút account
+    //Xu ly nút tìm kiếm 
+    var search_icon = document.getElementsByClassName("search-icon");
+    var search_right = document.getElementsByClassName("search-right");
+    var button_search_close = document.getElementById("button_search_close");
+    var bg_close_search = document.getElementById("bg-close-search");
+
+    search_icon[0].onclick = function () { search_right[0].style.display = "block"; };
+    search_icon[1].onclick = function () { search_right[0].style.display = "block"; };
+
+    var close_search_right = function () { search_right[0].style.display = "none"; };
+    button_search_close.onclick = function () { close_search_right(); };
+    bg_close_search.onclick = function () { close_search_right(); };
+
+    //Xu ly menu khi cuon chuot
+    //Xu ly khi cuon chuot
+    if ($(window).scrollTop() === 0)
+    {
+            $('.navbar').removeClass('denDam').addClass('denMo')
+            $('#back-to-top').addClass('anDi')
+    }
+
+    $(window).scroll(function ()
+    {
+            //Xy ly menuBar
+            if ($(this).scrollTop() < 400 || $(window).scrollTop() == 0)
+            {
+                    $('#menu_header').removeClass('fixed-menu-top');
+                    $('#menu_header_mobile').removeClass('fixed-menu-top');
+            }
+            else
+            {
+                $('#menu_header').addClass('fixed-menu-top');
+                $('#menu_header_mobile').addClass('fixed-menu-top');
+            }
+
+    });
+
 }, false);
